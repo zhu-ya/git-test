5fd7c03cb94d8834d71dc8a4e376e9d0802fc95f
Zhuyn <zhu_yn@yeah.net>
2021-05-17 21:45:37 +0800

    first commit

---
+++ src/main/java/com/atzhu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atzhu.git;
+
+/**
+ * @author zhu
+ * @create 2021-05-17 21:42
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+
+    }
+}
