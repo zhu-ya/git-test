ab38a60b3fb007291504ac732b9187b8d75c2267
Zhuyn <zhu_yn@yeah.net>
2021-05-17 21:49:25 +0800

    second commit

--- src/main/java/com/atzhu/git/GitTest.java
+++ src/main/java/com/atzhu/git/GitTest.java
@@ -7,6 +7,6 @@ package com.atzhu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-
+        System.out.println("hello hadoop");
     }
 }
